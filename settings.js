b8694d55c183761bdad4e0712b1cd2772f969197
awaiskhan7641515 <awaiskhan7641515@gmail.com>
2025-08-10 08:07:22 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -61,8 +61,6 @@ LINK_WHITELIST: "youtube.com,github.com",
 
         CUSTOM_REACT: process.env.CUSTOM_REACT || "true",
 // make this true for custum emoji react  
-  
-      CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝",
 // chose custom react emojis by yourself 
 
           DELETE_LINKS: process.env.DELETE_LINKS || "true",
